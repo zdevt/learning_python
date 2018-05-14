da580631a46a39e3a3ff01102f759bbb59612051
zdevt <zhangtao.hawk@qq.com>
2018-05-14 10:23:10 +0800

    add PyShow

---
+++ PyShow/00.py
@@ -0,0 +1,15 @@
+from PIL import Image,ImageFont,ImageDraw
+
+
+def add_num(img):
+    im = Image.open(img)
+    w,h = im.size
+    font = ImageFont.truetype("/Library/Fonts/Times New Roman Italic.ttf")
+    fillcolor = "#ff0000"
+    draw = ImageDraw.Draw(im)
+    draw.text((w-20,0),'1',font=font,fill=fillcolor)
+    im.save("a.jpg",'jpeg')
+
+
+if __name__ == '__main__' :
+    add_num("./PyShow/sven.jpg")
