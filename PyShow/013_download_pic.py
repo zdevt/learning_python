542b2eb977ec3f8b0e35646dabfb52c39ce6af08
zdevt <zhangtao.hawk@qq.com>
2018-05-17 16:18:40 +0800

    add 013 get pics

---
+++ PyShow/013_download_pic.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import os
+import requests
+
+from bs4 import BeautifulSoup
+
+url = 'http://tieba.baidu.com/p/2166231880'
+html = requests.get( url )
+soup = BeautifulSoup( html.text, 'html.parser' )
+img_urls = soup.findAll( 'img', bdwater = '杉本有美吧,1280,860' )
+
+os.makedirs('src013')
+
+for img_url in img_urls:
+    print(img_url)
+    img_src = img_url['src']
+    os.path.split( img_src )[1]
+    with open( 'src013/' + os.path.split( img_src )[1], 'wb' ) as f:
+        f.write( requests.get( img_src ).content )
