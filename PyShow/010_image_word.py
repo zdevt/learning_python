1ef91a43ae8cd6d7f4a5431913d43007c242f7a4
zdevt <zhangtao.hawk@qq.com>
2018-05-18 15:09:38 +0800

    add random char/number pig gen

---
+++ PyShow/010_image_word.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from PIL import Image, ImageDraw, ImageFont, ImageFilter
+import random
+
+
+def rdChar( ):
+    return chr( random.randint( 65, 90 ) )
+
+
+def rdColor( ):
+    return (random.randint( 64, 255 ), random.randint( 64, 255 ), random.randint( 64, 255 ))
+
+
+def rdColor2( ):
+    return (random.randint( 32, 127 ), random.randint( 32, 127 ), random.randint( 32, 127 ))
+
+
+width = 60 * 4
+height = 60
+
+image = Image.new( 'RGB', (width, height), (255, 255, 255) )
+font = ImageFont.truetype( '/Library/Fonts/AppleGothic.ttf', 36 )
+draw = ImageDraw.Draw( image )
+
+for x in xrange( width ):
+    for y in xrange( height ):
+        draw.point( (x, y), fill = rdColor( ) )
+
+for t in xrange( 4 ):
+    draw.text( (60 * t + 10, 10), rdChar( ), font = font, fill = rdColor2( ) )
+
+image = image.filter( ImageFilter.BLUR )
+image.save( '010.jpg', 'jpeg' )
+
+print(rdChar( ))
+print(rdColor( ))
--- PyShow/013_download_pic.py
+++ PyShow/013_download_pic.py
@@ -11,12 +11,12 @@ html = requests.get( url )
 soup = BeautifulSoup( html.text, 'html.parser' )
 img_urls = soup.findAll( 'img', bdwater = '杉本有美吧,1280,860' )
 
-shutil.rmtree('src013')
-os.makedirs('src013')
+shutil.rmtree('013_pic')
+os.makedirs('013_pic')
 
 for img_url in img_urls:
     print(img_url)
     img_src = img_url['src']
     os.path.split( img_src )[1]
-    with open( 'src013/' + os.path.split( img_src )[1], 'wb' ) as f:
+    with open( '013_pic/' + os.path.split( img_src )[1], 'wb' ) as f:
         f.write( requests.get( img_src ).content )
