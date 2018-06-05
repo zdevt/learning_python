900794d1750e73cc913b28d0c643045b2b05d5d7
zdevt <zhangtao.hawk@qq.com>
2018-06-05 16:13:57 +0800

    add itertools cycle

---
+++ pytest/itertools_cycle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+#       FileName:  t.py
+#
+#    Description:
+#
+#        Version:  1.0
+#        Created:  2018-06-05 16:08:52
+#  Last Modified:  2018-06-05 16:12:16
+#       Revision:  none
+#       Compiler:  gcc
+#
+#         Author:  zt ()
+#   Organization:
+
+from itertools import cycle
+
+playerIndexGen = cycle([0, 1, 2, 1])
+
+print(next(playerIndexGen))
+print(next(playerIndexGen))
+print(next(playerIndexGen))
+print(next(playerIndexGen))
+
+print(next(playerIndexGen))
+print(next(playerIndexGen))
+print(next(playerIndexGen))
+print(next(playerIndexGen))
